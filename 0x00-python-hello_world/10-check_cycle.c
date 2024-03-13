ab1fc1c6359c7e6fe88a9bd9bca3cc125d1f1f15
Mahmoud <mah2002moud@gmail.com>
2024-03-13 15:28:24 +0200

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -4,17 +4,17 @@
 
 /**
  * check_cycle - checks if the list contains a cycle
- * 
+ *
  * @list: the list
  * Return: 0 if there is no cycle
- *		   1 if contains cycle 
+ *		   1 if contains cycle
  */
 int check_cycle(listint_t *list)
 {
 	listint_t *ptr;
 
 	if (!list)
-		return(-1);
+		return (-1);
 
 	ptr = list;
 
