fe9fa3cb5bfff189670060d92543c02967aa8500
Mahmoud <mah2002moud@gmail.com>
2024-03-13 23:42:01 +0200

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+
+    uppered_string = ""
+
+    for char in str:
+        if ord(char) in range(ord('a'), ord('z') + 1):
+            uppered_string += chr(ord(char) - 32)
+        else:
+            uppered_string += char
+    return str
