e145b5a883d1dea4309cb445783e62fe9cb5a4d8
Mahmoud <mah2002moud@gmail.com>
2024-03-13 23:19:24 +0200

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(10):
     for j in range(i + 1, 10):
         if not (i == j):
             if not (i == 8 and j == 9):
-                print("{}{}".format(i,j), end=", ")
+                print("{}{}".format(i, j), end=", ")
             else:
-                print("{}{}".format(i,j))
+                print("{}{}".format(i, j))
