596f93db145a8c93e4c50091111a2981b62490fe
Mahmoud <mah2002moud@gmail.com>
2024-03-13 23:44:28 +0200

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -9,4 +9,4 @@ def uppercase(str):
             uppered_string += chr(ord(char) - 32)
         else:
             uppered_string += char
-    print(uppered_string)
+    print("{}".format(uppered_string))
