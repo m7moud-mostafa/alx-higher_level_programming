d5b3156c4cae794881bcd0a15a8b5351b1709d9b
Mahmoud <mah2002moud@gmail.com>
2024-03-21 23:38:51 +0200

    AlxProject

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_numerals = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+    int_val = 0
+
+    for i in range(len(roman_string)):
+        if (i > 0 and roman_numerals[roman_string[i]] >
+                roman_numerals[roman_string[i - 1]]):
+            int_val += (roman_numerals[roman_string[i]] -
+                        2 * roman_numerals[roman_string[i - 1]])
+        else:
+            int_val += roman_numerals[roman_string[i]]
+
+    return int_val
