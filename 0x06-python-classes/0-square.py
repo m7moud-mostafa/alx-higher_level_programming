6901beabed5cf717ed9c45edec89dd66d6f6d503
Mahmoud <mah2002moud@gmail.com>
2024-04-03 00:39:58 +0200

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
-"""This module defines an empty class square.
+"""
+This module defines an empty class square.
 The purpose of this class is to represent a square
-It contains one empty class called Square.
 """
 class Square:
-    """An empty class that defines a square.
-    To demonstrate the meaning of the class.
-    """
+    """Represents an empty Square prototype."""
     pass
