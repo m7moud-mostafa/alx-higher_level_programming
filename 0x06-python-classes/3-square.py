281fc78190c7e486bad55027b73d7dec829bebf5
Mahmoud <mah2002moud@gmail.com>
2024-04-03 01:27:09 +0200

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square with a private instance attribute size.
+"""
+
+
+class Square:
+    """
+    A class that represents a square.
+
+    Attributes:
+    size (int): The size of a side of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new Square with a given size.
+
+        Args:
+        size (int): The size of a side of the square.
+        """
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of the square"""
+        return self.__size ** 2
