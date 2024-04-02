b8f57694e3759334e995295863a38508d9a015a3
Mahmoud <mah2002moud@gmail.com>
2024-04-03 00:46:29 +0200

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class square.
+The purpose of this class is to represent a square
+"""
+class Square:
+    """Represents an empty Square prototype."""
+    def __init__(self, size=None):
+        """Init"""
+        self.__size = size
