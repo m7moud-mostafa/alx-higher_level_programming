1890bbe03299d5f8622e54b9465d284fc074caa4
Mahmoud <mah2002moud@gmail.com>
2024-04-02 16:01:23 +0200

    1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,12 +4,6 @@ def safe_print_integer(value):
 
     try:
         print("{:d}".format(value))
-        is_integer = True
+        return True
     except ValueError:
-        print("{} is not an integer".format(value))
-        is_integer = False
-    return is_integer
-
-
-if __name__ == "__main__":
-    safe_print_integer("kdj")
+        return False
