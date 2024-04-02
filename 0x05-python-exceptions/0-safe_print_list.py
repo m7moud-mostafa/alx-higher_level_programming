2cb214430cf6b4bb3dada3e2f6e13bb605d0402b
Mahmoud <mah2002moud@gmail.com>
2024-04-02 15:53:17 +0200

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,12 @@
 
 def safe_print_list(my_list=[], x=0):
     printed = 0
-    try:
-        for i in range(x):
+    for i in range(max(x, 1)):
+        try:
             print("{}".format(my_list[i]), end="")
             printed += 1
-    except IndexError:
-        pass
 
+        except IndexError:
+            break
     print("")
     return printed
