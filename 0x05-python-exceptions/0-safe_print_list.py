16eecba70e2a7159289ff4a5220e1ff83cb48a16
Mahmoud <mah2002moud@gmail.com>
2024-04-02 15:53:50 +0200

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 
 def safe_print_list(my_list=[], x=0):
     printed = 0
-    for i in range(max(x, 1)):
+    for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
             printed += 1
