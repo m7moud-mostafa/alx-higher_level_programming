4d71ab512b5c61148ea96a099b69939d787986b4
Mahmoud <mah2002moud@gmail.com>
2024-04-13 22:20:47 +0200

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,13 +7,13 @@ this module will contain the Rectangle class
 
 class Rectangle:
     """
-    this class does nothing
+    This Class is for Rectangle
     """
 
-    def __init__(self, width=0, hight=0):
+    def __init__(self, width=0, height=0):
         """Initialize the Class"""
         self.width = width
-        self.hight = hight
+        self.height = height
 
     @property
     def width(self):
@@ -30,15 +30,24 @@ class Rectangle:
         self.__width = value
 
     @property
-    def hight(self):
-        """Retrieves hight"""
-        return self.__hight
+    def height(self):
+        """Retrieves height"""
+        return self.__height
 
-    @hight.setter
-    def hight(self, value):
-        """Sets hight"""
+    @height.setter
+    def height(self, value):
+        """Sets height"""
         if not isinstance(value, int):
-            raise TypeError("hight must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("hight must be >= 0")
-        self.__hight = value
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle(2, 4)
+    print(my_rectangle.__dict__)
+
+    my_rectangle.width = 10
+    my_rectangle.height = 3
+    print(my_rectangle.__dict__)
