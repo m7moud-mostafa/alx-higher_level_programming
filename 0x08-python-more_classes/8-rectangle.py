e7753dce73ba57f84dfbd2c00f01137e598af187
Mahmoud <mah2002moud@gmail.com>
2024-04-14 00:22:40 +0200

    8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -81,7 +81,7 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
-    @classmethod
+    @staticmethod
     def bigger_or_equal(cls, rect_1, rect_2):
         """Returns the biggest rectangle"""
         if not isinstance(rect_1, Rectangle):
