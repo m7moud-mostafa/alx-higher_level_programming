89374585b12166b02c12e49c8703ba0d082eb964
Mahmoud <mah2002moud@gmail.com>
2024-04-16 23:05:27 +0200

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is an important module
+"""
+
+
+def write_file(filename="", text=""):
+    """reads a file"""
+    with open(filename, mode="a", encoding="utf-8") as file:
+        return file.write(text)
+
+
+if __name__ == "__main__":
+    print(write_file("my_first_file.txt", "This School is so cool!\n"))
