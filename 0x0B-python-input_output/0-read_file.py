6244cbb40cce3a8e96ba5dd5b3559642936e4cd7
Mahmoud <mah2002moud@gmail.com>
2024-04-16 18:43:09 +0200

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,7 @@ This is an important module
 def read_file(filename=""):
     """reads a file"""
     with open(filename, encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
+
+if __name__ == "__main__":
+    read_file("README.md")
