1f7d1352a21147a04a769de0f12950c9c8e9d928
Mahmoud <mah2002moud@gmail.com>
2024-04-16 23:05:47 +0200

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,11 +4,11 @@ This is an important module
 """
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """reads a file"""
     with open(filename, mode="a", encoding="utf-8") as file:
         return file.write(text)
 
 
 if __name__ == "__main__":
-    print(write_file("my_first_file.txt", "This School is so cool!\n"))
+    print(append_write("my_first_file.txt", "This School is so cool!\n"))
