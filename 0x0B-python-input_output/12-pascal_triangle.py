d9f69af81efe372d22538bdc27095bd7d77b10fc
Mahmoud <mah2002moud@gmail.com>
2024-04-17 04:59:19 +0200

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module contains the solution for
+the technical interview
+"""
+
+
+def pascal_triangle(n):
+	"""returns a list of lists of integers representing the Pascal’s triangle of n"""
+	if n <= 0:
+		return []
+	tri = [[1]]
+	for i in range(1, n):
+		row = [1]
+		for j in range(1, i):
+			row.append(tri[i - 1][j - 1] + tri[i - 1][j])
+		row.append(1)
+		tri.append(row)
+	return tri
