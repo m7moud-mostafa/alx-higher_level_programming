718692b244980448adba2572a8d12867d0369a6b
Mahmoud <mah2002moud@gmail.com>
2024-04-17 02:17:44 +0200

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     load_from_json_file = __import__("6-load_from_json_file")\
         .load_from_json_file
 
-    file_name = "add_items.json"
+    file_name = "add_item.json"
 
     try:
         json_list = load_from_json_file(file_name)
@@ -18,4 +18,4 @@ if __name__ == "__main__":
         json_list = []
 
     arg_list = sys.argv[1:]
-    save_to_json_file(json_list + arg_list, "add_items.json")
+    save_to_json_file(json_list + arg_list, file_name)
