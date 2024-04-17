977c2573b14933bf11ca708520d6669833b1098d
Mahmoud <mah2002moud@gmail.com>
2024-04-17 03:45:19 +0200

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module contains the Student Class
+"""
+
+
+class Student:
+	"""
+	Student Class contains the student name and age
+	"""
+
+	def  __init__(self, first_name, last_name, age):
+		"""Initializing the class"""
+		self.first_name = first_name
+		self.last_name = last_name
+		self.age = age
+
+	def to_json(self):
+		"""Retrieves a dict representation of Student instance"""
+		return Student.__dict__
