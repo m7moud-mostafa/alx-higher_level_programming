b7fe370eae551c396e49415e4096e69468117009
Mahmoud <mah2002moud@gmail.com>
2024-04-24 15:36:01 +0200

    5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -14,10 +14,14 @@ def text_indentation(text):
         if text[char] in ['.', '?', ':']:
             print(text[char])
             print("")
-            if char + 1 < len(text) and text[char + 1] == ' ':
-                char += 2
-            else:
-                char += 1
+            i = 1
+            while char + i < len(text) and text[char + i] == ' ':
+                i += 1
+            char += i
         else:
             print(text[char], end='')
             char += 1
+
+
+if __name__ == "__main__":
+    text_indentation("Holberton. School? How are you:    John")
