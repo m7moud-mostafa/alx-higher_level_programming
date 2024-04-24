e22bc12d82121d83cc410359fa79478325a69b03
Mahmoud <mah2002moud@gmail.com>
2024-04-24 14:04:39 +0200

    ./tests/0-add_integer.txt

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""
-Module to contain the add_integer(a, b=98) function.
-"""
+"""This module contains add_integer function"""
 
 
 def add_integer(a, b=98):
-    """Returns the addition of a and b"""
+    """adds to integers"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
+        
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return a + b
+
+    return (int(a) + int(b))
