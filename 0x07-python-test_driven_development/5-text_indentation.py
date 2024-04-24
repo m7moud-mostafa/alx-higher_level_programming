18b6e5e4b5712ab5e2eb3e4d942625e50a3eddc0
Mahmoud <mah2002moud@gmail.com>
2024-04-24 15:31:12 +0200

    5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """This module contains the text_indentation function"""
 
+
 def text_indentation(text):
-	"""
-	function that prints a text with 2 new lines
-	after each of these characters: `.`, `?` and `:`
-	"""
-	if not isinstance(text, str):
-		raise TypeError("text must be a string")
-	char = 0
-	while char < len(text):
-		if text[char] in ['.', '?', ':']:
-			print(text[char])
-			print("")
-			if char + 1 < len(text) and text[char + 1] == ' ':
-				char += 2
-			else:
-				char += 1
-		else:
-			print(text[char], end='')
-			char += 1
+    """
+    function that prints a text with 2 new lines
+    after each of these characters: `.`, `?` and `:`
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    char = 0
+    while char < len(text):
+        if text[char] in ['.', '?', ':']:
+            print(text[char])
+            print("")
+            if char + 1 < len(text) and text[char + 1] == ' ':
+                char += 2
+            else:
+                char += 1
+        else:
+            print(text[char], end='')
+            char += 1
