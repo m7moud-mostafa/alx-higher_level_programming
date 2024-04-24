c041071b6114ba22e72b0317e0cbe22cbd3e4f69
Mahmoud <mah2002moud@gmail.com>
2024-04-24 16:01:23 +0200

    ./tests/6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,36 +1,20 @@
 #!/usr/bin/python3
-"""
-Unittest for max_integer([..]).
-"""
+"""This is the file to test the max_integer function"""
 
 
 import unittest
-max_integer = __import__('6-max_integer').max_integer
 
+max_integer = __import__("6-max_integer").max_integer
 
-class TestMaxInteger(unittest.TestCase):
-    """
-    This class contains unittests for the max_integer function.
-    """
+class Max_integer_testing(unittest.TestCase):
+	"""
+	The unittest class to test the max_integer function
+	"""
+	def Value_test(self):
+		"""tests the value from the max_integer"""
+		self.assertEqual(max_integer([4, 15, 3, 6, 6, 3]), 15)
 
-    def test_max_integer(self):
-        """Test method for max integer in a list."""
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
-        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
-        self.assertEqual(max_integer([]), None)
-        self.assertEqual(max_integer([1]), 1)
-        self.assertIsNone(max_integer([]))
-        self.assertEqual(max_integer([-2, -4, -6, -8]), -2)
-
-    def test_type_errors(self):
-        """Test method for type errors."""
-        with self.assertRaises(TypeError):
-            max_integer(None)
-        with self.assertRaises(TypeError):
-            max_integer(10)
-        with self.assertRaises(TypeError):
-            max_integer("string")
-
-if __name__ == '__main__':
-    unittest.main()
+	def type_test(self):
+		"""tests for types errors"""
+		self.assertRaises(max_integer(5), TypeError)
+		self.assertRaises(max_integer([4, 5, 6, 6, "mahmoud"]), TypeError)
