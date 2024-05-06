e7f236868984a0e1df9b7e91563c2a7e68e01b94
Mahmoud <mah2002moud@gmail.com>
2024-05-06 23:01:20 +0300

    , 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -10,14 +10,3 @@ class MyList(list):
 		self.l = self.copy()
 		self.l.sort()
 		print(self.l)
-
-
-my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
-print(my_list)
-my_list.print_sorted()
-print(my_list)
