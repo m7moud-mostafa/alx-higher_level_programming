0bde22e351fe7c8dd09b19617897a82b8208f39d
Mahmoud <mah2002moud@gmail.com>
2024-05-06 23:45:17 +0300

    Added files:    4-inherits_from.py      2-is_same_class.py      3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -8,3 +8,13 @@ def is_kind_of_class(obj, a_class):
     an instance of the specified class
     """
     return isinstance(obj, a_class)
+
+
+if __name__ == "__main__":
+    a = 1
+    if is_kind_of_class(a, int):
+        print("{} comes from {}".format(a, int.__name__))
+    if is_kind_of_class(a, float):
+        print("{} comes from {}".format(a, float.__name__))
+    if is_kind_of_class(a, object):
+        print("{} comes from {}".format(a, object.__name__))
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-"""
-This module contains many functions
-"""
+"""This module contains the inherits_from function"""
 
 
 def inherits_from(obj, a_class):
     """
-    Check if the object is an instance of a class that inherited (directly or indirectly) from the specified class.
+    Check if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class.
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
+
+
+if __name__ == "__main__":
+    a = True
+    if inherits_from(a, int):
+        print("{} inherited from class {}".format(a, int.__name__))
+    if inherits_from(a, bool):
+        print("{} inherited from class {}".format(a, bool.__name__))
+    if inherits_from(a, object):
+        print("{} inherited from class {}".format(a, object.__name__))
