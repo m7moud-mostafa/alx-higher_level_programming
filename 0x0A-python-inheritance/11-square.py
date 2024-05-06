f2c6e0774d4004717beed630a8ae3da196f3c2f6
Mahmoud <mah2002moud@gmail.com>
2024-05-07 01:01:36 +0300

    Added files:    11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
-"""
-This module contains many functions
-"""
+"""This module contains the Square class"""
 
 
 Rectangle = __import__("9-rectangle").Rectangle
+
+
 class Square(Rectangle):
-    """
-    Class Square that inherits from Rectangle. Redefines str() to return square-specific description.
-    """
+    """Square Class"""
+
     def __init__(self, size):
-        """
-        Instantiates with size. Size must be a positive integer, validated by integer_validator.
-        """
-        super().__init__(size, size)  # Size passed as both width and height
-    
-    def area(self):
-        """
-        Returns the area of the square.
-        """
-        return self._Rectangle__width * self._Rectangle__height
+        """Initializing the Square class"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
 
     def __str__(self):
-        """
-        Returns the square description in the format [Square] <width>/<height>
-        """
-        return f"[Square] {self._Rectangle__width}/{self._Rectangle__height}"
+        """Returns a description about the object"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
+
+if __name__ == "__main__":
+
+    s = Square(13)
+
+    print(s)
+    print(s.area())
