fea1e76bef76756755b9f49ef293ff0fb6ce9bea
Mahmoud <mah2002moud@gmail.com>
2024-05-07 19:08:37 +0300

    Added files:    models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -80,6 +80,10 @@ class Rectangle(Base):
         for i in range(self.height):
             print("#" * self.width)
 
+    def __str__(self):
+        """returns a description """
+        return ("({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height))
+
 
 if __name__ == "__main__":
     r1 = Rectangle(4, 6)
