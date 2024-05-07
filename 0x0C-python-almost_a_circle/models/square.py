79c7e451728b4b69cd485c98a31faf0627a7b1d7
Mahmoud <mah2002moud@gmail.com>
2024-05-07 20:54:37 +0300

    Added files:    models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,8 +8,9 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initializing the Square class"""
-        super().__init__(size, size, x, y, id)
         self.size = size
+        super().__init__(size, size, x, y, id)
+
 
     @property
     def size(self):
