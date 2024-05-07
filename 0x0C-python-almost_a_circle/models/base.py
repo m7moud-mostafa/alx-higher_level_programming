0a365a5cc479a5d510901a1446bb261eb2eecf7a
Mahmoud <mah2002moud@gmail.com>
2024-05-07 22:12:45 +0300

    Added files:    models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -42,6 +42,15 @@ class Base:
             return []
         return json.loads(json_string)
 
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy_instance = cls(1)
+
+        dummy_instance.update(**dictionary)
+        return dummy_instance
 
 if __name__ == "__main__":
 
