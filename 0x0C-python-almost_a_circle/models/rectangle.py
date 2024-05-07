ba84078648dd4bb2a79987a192d0a1f2898b9bd2
Mahmoud <mah2002moud@gmail.com>
2024-05-07 21:08:02 +0300

    Added files:    models/rectangle.py      models/square.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module contains the rectangle class"""
-from models.base import Base
+from base import Base
 
 
 class Rectangle(Base):
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,39 +8,29 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initializing the Square class"""
-        self.size = size
         super().__init__(size, size, x, y, id)
-
 
     @property
     def size(self):
         """getter for size"""
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, value):
         """setter for size"""
-        self.__integer_validator("size", value)
-        self.__size = value
+        self.width = value
+        self.height = value
 
 
 if __name__ == "__main__":
 
     s1 = Square(5)
     print(s1)
-    print(s1.area())
-    s1.display()
+    print(s1.size)
+    s1.size = 10
+    print(s1)
 
-    print("---")
-
-    s2 = Square(2, 2)
-    print(s2)
-    print(s2.area())
-    s2.display()
-
-    print("---")
-
-    s3 = Square(3, 1, 3)
-    print(s3)
-    print(s3.area())
-    s3.display()
+    try:
+        s1.size = "9"
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
