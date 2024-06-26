d175f40813b8c91c107cd8acf6323135bc4ccbff
Mahmoud <mah2002moud@gmail.com>
2024-06-26 12:19:22 +0300

    Added files:    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    query = "SELECT (c.id, c.name, s.name) FROM cities AS c \
+    query = "SELECT c.id, c.name, s.name FROM cities AS c \
         	 LEFT JOIN states AS s ON s.id = c.state_id \
              ORDER BY c.id ASC"
     cur.execute(query)
