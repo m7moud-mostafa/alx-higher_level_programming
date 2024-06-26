38fb51a60dc917742506768a9f595a93960e918a
Mahmoud <mah2002moud@gmail.com>
2024-06-26 12:09:44 +0300

    Added files:    4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""lists states from the database"""
+
+
+if __name__ == "__main__":
+    import MySQLdb as sql
+    import sys
+
+    db = sql.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    cur = db.cursor()
+    query = "SELECT (c.id, c.name, s.name) FROM cities AS c \
+        	 LEFT JOIN states AS s ON s.id = c.state_id \
+             ORDER BY c.id ASC"
+    cur.execute(query)
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
