51aad114986a14a4eaa6c5f077ff0c7fd87de396
Mahmoud <mah2002moud@gmail.com>
2024-06-26 11:52:11 +0300

    Added files:    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""lists states from the database"""
+
+
+if __name__ == "__main__":
+    import MySQLdb as sql
+    import sys
+
+    db = sql.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    cur = db.cursor()
+    name = sys.argv[4].split()[0]
+    query = "SELECT * FROM states WHERE BINARY name='{}' \
+             ORDER BY id ASC".format(name)
+    cur.execute(query)
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
