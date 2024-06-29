79f9e78bcf54185cfd7607cc66056f35f1b6c315
Mahmoud <mah2002moud@gmail.com>
2024-06-29 22:42:23 +0300

    Added files:    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -26,9 +26,10 @@ if __name__ == "__main__":
 
     states = session.query(State)\
         .filter(State.name == name).order_by(State.id)
-
+    x = 0
     if states:
         for state in states:
-            print("{}: {}".format(state.id, state.name))
+            x += 1
+        print(x)
     else:
         print("Not found")
