ace177e6bb34128fe2f76452f68c2453e4b77553
Mahmoud <mah2002moud@gmail.com>
2024-06-29 22:33:15 +0300

    Added files:    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
               <mysql_password> <database_name> <name>")
         sys.exit(1)
 
-    name = sys.argv[4].split()[1]
+    name = sys.argv[4].split(maxsplit=1)
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
