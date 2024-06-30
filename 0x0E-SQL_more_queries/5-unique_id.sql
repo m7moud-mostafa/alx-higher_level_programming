b9f3855a9e7cb18f706092113f546889c92716d4
Mahmoud <mah2002moud@gmail.com>
2024-06-30 14:13:32 +0300

    Added files:    5-unique_id.sql

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,5 @@
---create the table unique
-CREATE TABLE IF NOT EXISTS unique_id(
-	id INT DEFAULT 1 UNIQUE,
-	name VARCHAR(256)
-
+-- Create the table unique_id if it does not exist
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT NOT NULL DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
 );
