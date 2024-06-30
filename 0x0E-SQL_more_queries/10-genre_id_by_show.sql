156ad07d96858577fa9fbc7088073e7f66380f44
Mahmoud <mah2002moud@gmail.com>
2024-06-30 18:15:03 +0300

    Added files:    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
---script that lists all shows contained in hbtn_0d_tvshows
-SELECT tv_shows.title, tv_shows_genres.genre_id
+-- Script that lists all shows contained in hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_shows_genres
-ON tv_shows.id = tv_shows_genres.show_id
-ORDER BY tv_show.id ASC, tv_shows_genres.genre_id ASC;
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.id ASC, tv_show_genres.genre_id ASC;
