5911113399eae732faf1df01a17f459b07726359
Mahmoud <mah2002moud@gmail.com>
2024-06-30 14:19:41 +0300

    Added files:    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,8 @@
 -- Creates the database hbtn_0d_usa and states table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS states(
 	id INT UNIQUE AUTO_INCREMENT NOT NULL,
 	name VARCHAR(256) NOT NULL,
